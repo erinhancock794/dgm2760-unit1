4c1260ade62d826a70845b61b00c4d67cb19e2f7
erinhancock794 <erinhancock794@gmail.com>
2020-01-23 00:05:56 -0700

    added css styling to page

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ document.querySelector('#company').innerText = "Joe's Bed & Breakfast"
  
 document.querySelector('header > h2').innerText = "Coziest B&B in Utah!"
 
-//let userName = prompt("What is your name?")
-//let message = `Hello ${userName}, welcome to the most comfortable place to stay in Utah!`
+let userName = prompt("What is your name?")
+let message = `Hello ${userName}! Welcome to the most comfortable place to stay in Utah!`
 
-//document.querySelector('#greeting').innerText = message
+document.querySelector('#greeting').innerText = message
